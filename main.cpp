def4e264d05ffa2c5f02d801597bfc4ab5013bd0
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2022-06-14 12:17:02 -0300

    Cria menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <iostream>
+#include <string.h>
 
-void main(){
+using namespace std;
 
+int main(){
+    string entrada = "  ";
+    while(entrada != ":q"){
+        cin >> entrada;
+        //menu
+        if(entrada == ":d"){
+            //dividir arquivo de entrada em tags
+            cout << "[WARNING] Funcao nao implementada";
 
+        }else if(entrada == ":c"){
+            //carregar tags de um arquivo
+
+        }else if(entrada == ":o"){
+            //definir output
+            
+        }else if(entrada == ":p"){
+            //dividir entrada em tags
+            cout << "[WARNING] Funcao nao implementada";
+
+        }else if(entrada == ":a"){
+            //listar autômatos
+            
+        }else if(entrada == ":l"){
+            //listar tags válidas
+            
+        }else if(entrada == ":s"){
+            //salvar tag
+            
+        }else if(entrada == ":q"){
+            //encerrar programa
+            break;
+            
+        }else{
+            //verificar e criar tag
+
+        }
+    }
     
 }
